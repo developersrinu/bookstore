e3a3c45732a95e173e167e588d6c039b24c41ce3
developersrinu <orsusrinu606@gmail.com>
2023-08-26 20:43:44 +0530

    mmm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Book from './components/Book';
 
 function App() {
+  // State to hold the initial book data f
+
+  const [initialBooks, setInitialBooks] = useState([]);
+
+  // Fetch initial book data when the component mounts
+  useEffect(() => {
+    axios.get('https://www.googleapis.com/books/v1/volumes?q=harry+potter')
+      .then((response) => {
+        // Extract the first three books from the API response
+        const firstThreeBooks = response.data.items.slice(0, 3);
+        // Set the initial book data in the state
+        setInitialBooks(firstThreeBooks);
+      })
+      .catch((error) => {
+        console.error('Error fetching books:', error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Display the list of books */}
+        <div className="books">
+          {initialBooks.map((book, index) => (
+            // Render the Book component for each book
+            <Book key={index} book={book} />
+          ))}
+        </div>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
