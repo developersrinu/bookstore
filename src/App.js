6bf5ec5897937c9ff4388ffa6a6594408b5a59a3
developersrinu <orsusrinu606@gmail.com>
2023-08-26 20:50:52 +0530

    nnn

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './App.css';
-import Book from './components/Book';
+import './App.css'; // You can define your styles in this file
 
 function App() {
-  // State to hold the initial book data f
-
   const [initialBooks, setInitialBooks] = useState([]);
 
-  // Fetch initial book data when the component mounts
   useEffect(() => {
     axios.get('https://www.googleapis.com/books/v1/volumes?q=harry+potter')
       .then((response) => {
-        // Extract the first three books from the API response
         const firstThreeBooks = response.data.items.slice(0, 3);
-        // Set the initial book data in the state
         setInitialBooks(firstThreeBooks);
       })
       .catch((error) => {
@@ -25,19 +19,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* Display the list of books */}
-        <div className="books">
-          {initialBooks.map((book, index) => (
-            // Render the Book component for each book
-            <Book key={index} book={book} />
-          ))}
-        </div>
+        {/* Display your books */}
       </header>
     </div>
   );
 }
 
 export default App;
+
 
 
 
